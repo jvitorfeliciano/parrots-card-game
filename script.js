9d4af9119a450069b65ae0561213d70686e4983a
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-08-20 21:26:50 -0300

    versão com a vaerificação das cartas

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let cardNumbers=0;
-
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+let cardNumbers=0;
+let count4=0;
 const imgNumber = [0,1,2,3,4,5,6];
 imgNumber.sort(comparador);
 const imgArchive =[];
-
+let cardFlipped = [];
 function start(){
 while(cardNumbers<4 || cardNumbers >14 || cardNumbers%2 !=0){
 
@@ -19,18 +19,18 @@ start();
 function cardOrganization (){
     let count1 =0;
    while(count1 < cardNumbers/2){
-        imgArchive.push (`<div class="card C${count1}"}>
-        <div class="front-face face" onclick="">
+        imgArchive.push (`<div class="card" cardNumber ="C${count1}"  onclick="flipCard(this)">
+        <div class="face1" >
             <img src="./imagens/front.png" alt="">
         </div>
-        <div class="back-face face">
+        <div class="back face2">
         <img src="./imagens/${imgNumber[count1]}.gif" alt="">
         </div> `)
-        imgArchive.push (`<div class="card  C${count1}" >
-        <div class="front-face face" onclick="">
+        imgArchive.push (`<div class="card" onclick="flipCard(this)" cardNumber ="C${count1}">
+        <div class="face1" >
             <img src="./imagens/front.png" alt="">
         </div>
-        <div class="back-face face">
+        <div class="back  face2">
         <img src="./imagens/${imgNumber[count1]}.gif" alt="">
         </div> `)  
         count1++
@@ -40,13 +40,58 @@ function cardOrganization (){
 cardOrganization();
 
 function cardDistribution (){
-let count2 = 0;
-const addCard = document.querySelector('.contents');
-addCard.innerHTML="";
-while (count2< cardNumbers){
-    addCard.innerHTML = addCard.innerHTML + imgArchive[count2]
-    count2++
+    let count2 = 0;
+    const addCard = document.querySelector('.contents');
+    addCard.innerHTML="";
+    while (count2< cardNumbers){
+        addCard.innerHTML = addCard.innerHTML + imgArchive[count2]
+        count2++
 }
 }
 cardDistribution();
 
+function flipCard(card){
+    
+    const front = card.querySelector('.face1')
+    front.classList.add('front-face')
+    const back = card.querySelector('.face2') 
+    back.classList.add('back-face') 
+    cardFlipped.push(card)
+    count4 ++
+
+    if (count4%2==0){
+        verification();
+        
+    }  
+}
+
+function remove(){
+    for (let i=0; i<cardFlipped.length; i++){
+        cardFlipped[i].querySelector('.front-face').classList.remove('front-face')
+        cardFlipped[i].querySelector('.back-face').classList.remove('back-face')
+    }
+
+    cardFlipped.splice(0, cardFlipped.length)
+}
+
+
+function verification() {
+    const card1 = cardFlipped[0].getAttribute('cardNumber')
+    const card2 = cardFlipped[1].getAttribute('cardNumber')
+    if (card1 !== card2){
+        setTimeout(remove, 1000)
+        } else{
+            cardFlipped.splice(0, cardFlipped.length)
+        }
+        
+    }
+        
+
+    
+
+
+
+
+
+
+      
