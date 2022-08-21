f4de69c968ba2c8eabbf9d89c87844b67e3cad4b
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-08-21 01:04:55 -0300

    versão com o contador de jogadas finalizado

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 let cardNumbers=0;
-let count4=0;
+let count1=0;
 const imgNumber = [0,1,2,3,4,5,6];
 imgNumber.sort(comparador);
 const imgArchive =[];
+const imgPairs =[];
 let cardFlipped = [];
+
 function start(){
 while(cardNumbers<4 || cardNumbers >14 || cardNumbers%2 !=0){
 
@@ -17,76 +19,75 @@ while(cardNumbers<4 || cardNumbers >14 || cardNumbers%2 !=0){
 start();
 
 function cardOrganization (){
-    let count1 =0;
-   while(count1 < cardNumbers/2){
-        imgArchive.push (`<div class="card" cardNumber ="C${count1}"  onclick="flipCard(this)">
+   for(let i=0; i<cardNumbers/2; i++){
+    imgArchive.push (`<div class="card" cardNumber ="C${i}"  onclick="flipCard(this)">
         <div class="face1" >
             <img src="./imagens/front.png" alt="">
         </div>
         <div class="back face2">
-        <img src="./imagens/${imgNumber[count1]}.gif" alt="">
-        </div> `)
-        imgArchive.push (`<div class="card" onclick="flipCard(this)" cardNumber ="C${count1}">
+        <img src="./imagens/${imgNumber[i]}.gif" alt="">
+        </div> `);
+     imgArchive.push (`<div class="card" onclick="flipCard(this)" cardNumber ="C${i}">
         <div class="face1" >
             <img src="./imagens/front.png" alt="">
         </div>
         <div class="back  face2">
-        <img src="./imagens/${imgNumber[count1]}.gif" alt="">
-        </div> `)  
-        count1++
-     }
+        <img src="./imagens/${imgNumber[i]}.gif" alt="">
+        </div> `); 
+   }  
      imgArchive.sort(comparador);
 }
 cardOrganization();
 
 function cardDistribution (){
-    let count2 = 0;
     const addCard = document.querySelector('.contents');
     addCard.innerHTML="";
-    while (count2< cardNumbers){
-        addCard.innerHTML = addCard.innerHTML + imgArchive[count2]
-        count2++
+    for (let i=0; i<cardNumbers; i++){
+        addCard.innerHTML = addCard.innerHTML + imgArchive[i]; 
+    }
 }
-}
+
 cardDistribution();
 
 function flipCard(card){
-    
-    const front = card.querySelector('.face1')
-    front.classList.add('front-face')
-    const back = card.querySelector('.face2') 
-    back.classList.add('back-face') 
-    cardFlipped.push(card)
-    count4 ++
-
-    if (count4%2==0){
-        verification();
-        
+    const front = card.querySelector('.face1');
+    front.classList.add('front-face');
+    const back = card.querySelector('.face2') ;
+    back.classList.add('back-face') ;
+    cardFlipped.push(card);
+    count1 ++
+    if (count1%2==0){
+        verification();  
     }  
+
+    setTimeout(endGame,500);
 }
 
 function remove(){
     for (let i=0; i<cardFlipped.length; i++){
-        cardFlipped[i].querySelector('.front-face').classList.remove('front-face')
-        cardFlipped[i].querySelector('.back-face').classList.remove('back-face')
+        cardFlipped[i].querySelector('.front-face').classList.remove('front-face');
+        cardFlipped[i].querySelector('.back-face').classList.remove('back-face');
     }
-
-    cardFlipped.splice(0, cardFlipped.length)
+    cardFlipped.splice(0, cardFlipped.length);
 }
 
-
 function verification() {
-    const card1 = cardFlipped[0].getAttribute('cardNumber')
-    const card2 = cardFlipped[1].getAttribute('cardNumber')
+    const card1 = cardFlipped[0].getAttribute('cardNumber');
+    const card2 = cardFlipped[1].getAttribute('cardNumber');
     if (card1 !== card2){
-        setTimeout(remove, 1000)
+        setTimeout(remove, 1000);
         } else{
-            cardFlipped.splice(0, cardFlipped.length)
-        }
-        
+            imgPairs.push(cardFlipped[0])
+            imgPairs.push(cardFlipped[1])
+            cardFlipped.splice(0, cardFlipped.length);
+        }     
     }
         
-
+function endGame (){
+    if (imgPairs.length === cardNumbers){
+        alert(`Você ganhou com ${count1} jogadas`);
+    }
+}
     
 
 
